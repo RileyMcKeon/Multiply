b571e7a84c372539202b1decbf9775dc441e6724
Steven Anderson <sande139@d127.org>
2022-11-14 09:43:02 -0600

    first connections

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
