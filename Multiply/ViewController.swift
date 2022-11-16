d7a7e12a78f2be965042b57ed2ba09d3a260c0cc
Steven Anderson <sande139@d127.org>
2022-11-16 10:20:22 -0600

    button and comments

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -13,11 +13,29 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var answerLabel: UILabel!
     
+    @IBOutlet weak var textField1: UITextField!
+    
+    @IBOutlet weak var textField2: UITextField!
+    
+    @IBOutlet weak var multiplyButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func mulitplyWhenPressed(_ sender: UIButton) {
+//        MVP - Riley
+        
+//        Stretch Two and Three - Makayla
+        
+        
+//        Stretch One - Steven
+        
+        
+//        Stretch Four - Steven
 
+    }
+    
 }
 
